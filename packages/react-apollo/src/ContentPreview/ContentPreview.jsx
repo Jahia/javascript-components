9a068805630b37fad2279fa2fbd79a6c8ba521da
Kevan <kevan@jahia.com>
2019-09-20 10:39:30 +0200

    BACKLOG-11111: handle request attributes on preview query

--- packages/react-apollo/src/ContentPreview/ContentPreview.jsx
+++ packages/react-apollo/src/ContentPreview/ContentPreview.jsx
@@ -4,14 +4,15 @@ import {Query} from 'react-apollo';
 import {lodash as _} from 'lodash';
 import {previewQuery} from './ContentPreview.gql-queries';
 
-const ContentPreview = ({path, workspace, language, templateType, view, contextConfiguration, setRefetch, fetchPolicy, children}) => {
+const ContentPreview = ({path, workspace, language, templateType, view, contextConfiguration, requestAttributes, setRefetch, fetchPolicy, children}) => {
     const queryVariables = {
         path: path,
         templateType: templateType,
         view: view,
         contextConfiguration: contextConfiguration,
         language: language,
-        workspace: workspace.toUpperCase()
+        workspace: workspace.toUpperCase(),
+        requestAttributes: requestAttributes
     };
 
     return (
@@ -47,7 +48,8 @@ const ContentPreview = ({path, workspace, language, templateType, view, contextC
 
 ContentPreview.defaultProps = {
     setRefetch: null,
-    fetchPolicy: 'cache-first'
+    fetchPolicy: 'cache-first',
+    requestAttributes: null
 };
 
 ContentPreview.propTypes = {
@@ -58,6 +60,7 @@ ContentPreview.propTypes = {
     templateType: PropTypes.string.isRequired,
     view: PropTypes.string.isRequired,
     contextConfiguration: PropTypes.string.isRequired,
+    requestAttributes: PropTypes.arrayOf(PropTypes.object),
     setRefetch: PropTypes.func,
     fetchPolicy: PropTypes.string
 };
