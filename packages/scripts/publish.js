a1f5d6fa174c527afefc21318474f6f2f0c3c5f1
Thomas Draier <tdraier@jahia.com>
2019-12-19 16:06:50 +0100

    BACKLOG-11783 : Use old git syntax, push all tags

--- packages/scripts/publish.js
+++ packages/scripts/publish.js
@@ -80,7 +80,7 @@ console.log('Publishing ..');
 spawnSync('yarn', ['publish', buildPath, '--no-git-tag-version', '--new-version', newVersion.substr(1)]);
 
 // Push to repository
-spawnSync('git', ['push', '--no-verify', '--follow-tags']);
+spawnSync('git', ['push', '--no-verify', '--tags']);
 
 // Do git release
 spawnSync('yarn', ['auto', 'release', '--use-version', tag, '--from', previous]);
