98b5e8d36d08b51e6d8e9e3c2bf814bba8af4704
Thomas Draier <tdraier@jahia.com>
2019-12-19 17:38:42 +0100

    BACKLOG-11783 : Use "auto" for releases (#60)
    * BACKLOG-11783 : Use auto for release/publish scripts
    
    * BACKLOG-11783: auto config and plugin
    
    * BACKLOG-11783 : Commit version change
    
    * BACKLOG-11783 : Fixed tag name
    
    * BACKLOG-11783 : Get previous tag name
    
    * BACKLOG-11783 : Fixed project/tag name
    
    * BACKLOG-11783 : Fixed publish command
    
    * BACKLOG-11783 : Read env from root folder, added auto config for all packages
    
    * BACKLOG-11783 : Skip release when no label is present
    
    * BACKLOG-11783 : Avoid creation of changelog , skip when not on master branch
    
    * BACKLOG-11783 : Skip release when there's no commit
    
    * Update packages/scripts/auto-include-plugin.js
    
    Co-Authored-By: Florent Berthelot <FBerthelot@users.noreply.github.com>
    
    * BACKLOG-11783 : Fixed suggestion
    
    * BACKLOG-11783 : Use old git syntax, push all tags
    
    * BACKLOG-11783 : Enable npm plugin for canary/shipit

---
+++ packages/scripts/auto-include-plugin.js
@@ -0,0 +1,36 @@
+Object.defineProperty(exports, '__esModule', {value: true});
+
+/** Ensure a value is an array */
+const arrayify = arr => (Array.isArray(arr) ? arr : [arr]);
+class FilterCommitsByFolderPlugin {
+    /** Initialize the plugin with it's options */
+    constructor(options) {
+        /** The name of the plugin */
+        this.name = 'Include only commits which include at least a file in one of the specified folders';
+        this.options = {
+            paths: options.paths ? arrayify(options.paths) : []
+        };
+    }
+
+    /** Tap into auto plugin points. */
+    apply(auto) {
+        auto.hooks.onCreateLogParse.tap(this.name, logParse => {
+            logParse.hooks.omitCommit.tap(this.name, commit => {
+                // Remove current working directory which is added by a path.resolve
+                // https://github.com/intuit/auto/blob/v8.6.0/packages/core/src/git.ts:309
+                const files = commit.files
+                    .map(f => f.substr(process.cwd().length + 1))
+                    .filter(f => this.options.paths.filter(p => f.startsWith(p)).length > 0); // Keep only files in the requested folder
+
+                // Output the commit hash - needed to count the accepted commits
+                if (files.length > 0) {
+                    console.log(commit.hash);
+                }
+
+                // Skip (return true) if no file in the commit matches one of the base path
+                return files.length === 0;
+            });
+        });
+    }
+}
+exports.default = FilterCommitsByFolderPlugin;
--- packages/scripts/publish.js
+++ packages/scripts/publish.js
@@ -1,48 +1,79 @@
 #!/usr/bin/env node
 const path = require('path');
 const fs = require('fs');
-const yarn = require('yarn-api');
 const branch = require('git-branch');
-const util = require('util');
+const spawn = require('cross-spawn');
+const dotenv = require('dotenv');
 
 const json = require(path.resolve('./package.json'));
 const branchName = branch.sync();
 
-let build = json.version;
+let projectName = json.name.substring(json.name.lastIndexOf('/') + 1);
+let previous = projectName + '-v' + json.version;
+let buildPath = fs.existsSync('build/package.json') ? 'build' : '.';
 
-let params = [];
+dotenv.config({path: '../../.env'});
 
-if (fs.existsSync('build/package.json')) {
-    params.push('build');
-} else {
-    params.push('.');
-}
+const spawnSync = (command, params, options) => {
+    let subprocess = spawn.sync(command, params, options);
 
-let name = 'beta';
-if (branchName.startsWith('feature-')) {
-    name = branchName.replace(/-/g, '');
-    params.push('--tag', name);
-}
-
-build += '-' + name + '.' + (new Date()).toISOString().slice(0, 19).replace(/[-:T]/g, '');
-
-console.log(build);
-params.push('--no-git-tag-version', '--new-version', build);
-
-const yarnPromise = util.promisify(yarn);
-
-async function yarnSync(params) {
-    let filePath = path.join('.', params[1], 'package.json');
-    let previous = fs.readFileSync(filePath);
-
-    let d = await yarnPromise(params);
-    if (branchName.startsWith('feature-')) {
-        await yarnPromise(['tag', 'add', json.name + '@' + build, name]);
+    if (subprocess.stdout) {
+        console.log(subprocess.stdout.toString('ascii'));
     }
 
-    fs.writeFileSync(filePath, previous);
+    if (subprocess.status !== 0) {
+        if (subprocess.stderr) {
+            console.log('ERROR : ' + subprocess.stderr.toString('ascii'));
+        }
 
-    return d;
+        process.exit(subprocess.status);
+    }
+
+    return subprocess;
+};
+
+console.log('Releasing code from branch : ' + branchName);
+
+// Ensure code is called from master
+if (branchName !== 'master') {
+    console.log('Can only release from master branch');
+    process.exit(1);
 }
 
-yarnSync(['publish', ...params]);
+// Get new version
+const autoVersionProcess = spawnSync('yarn', ['auto', 'version', '--from', previous]);
+const lines = autoVersionProcess.stdout.toString('ascii').split(/\r?\n/);
+
+const commitsCount = lines.length - 3;
+const versionChange = lines[lines.length - 2];
+
+if (commitsCount === 0 || (versionChange !== 'patch' && versionChange !== 'minor' && versionChange !== 'major')) {
+    console.log('No new version detected');
+    process.exit(0);
+}
+
+// Generate changelog
+spawnSync('yarn', ['auto', 'changelog', '--from', previous]);
+
+// Bump version
+console.log('Auto version change : ' + versionChange);
+const npmVersionProcess = spawnSync('npm', ['version', versionChange]);
+const newVersion = npmVersionProcess.stdout.toString().split(/\r?\n/)[0];
+let tag = projectName + '-' + newVersion;
+console.log('Will release ' + tag);
+
+spawnSync('git', ['add', 'package.json']);
+spawnSync('git', ['commit', '-n', '-m', 'Bump ' + projectName + ' version to: ' + newVersion + ' [skip ci]']);
+
+console.log('Publishing ..');
+
+// Publish
+spawnSync('yarn', ['publish', buildPath, '--no-git-tag-version', '--new-version', newVersion.substr(1)]);
+
+// Push to repository
+spawnSync('git', ['push', '--no-verify', '--tags']);
+
+// Do git release
+spawnSync('yarn', ['auto', 'release', '--use-version', tag, '--from', previous]);
+
+console.log('Done');
