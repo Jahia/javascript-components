de1944716b6013c501905dac9f7c08193e370546
Thomas Draier <tdraier@jahia.com>
2019-12-19 10:51:40 +0100

    BACKLOG-11783 : Use auto for release/publish scripts

--- packages/scripts/publish.js
+++ packages/scripts/publish.js
@@ -1,48 +1,75 @@
 #!/usr/bin/env node
 const path = require('path');
 const fs = require('fs');
-const yarn = require('yarn-api');
 const branch = require('git-branch');
-const util = require('util');
+const spawn = require('cross-spawn');
 
 const json = require(path.resolve('./package.json'));
 const branchName = branch.sync();
 
+let projectName = json.name.substring(json.name.lastIndexOf('/') + 1);
 let build = json.version;
+let tag = projectName + '-v' + build;
+let previous = 'design-system-kit-v1.0.0';
+let buildPath = fs.existsSync('build/package.json') ? 'build' : '.';
 
-let params = [];
+const spawnSync = (command, params, options) => {
+    let subprocess = spawn.sync(command, params, options);
+    if (subprocess.status !== 0) {
+        if (subprocess.stdout) {
+            console.log(subprocess.stdout.toString('ascii'));
+        }
 
-if (fs.existsSync('build/package.json')) {
-    params.push('build');
-} else {
-    params.push('.');
-}
+        if (subprocess.stderr) {
+            console.log('ERROR : ' + subprocess.stderr.toString('ascii'));
+        }
 
-let name = 'beta';
-if (branchName.startsWith('feature-')) {
-    name = branchName.replace(/-/g, '');
-    params.push('--tag', name);
-}
-
-build += '-' + name + '.' + (new Date()).toISOString().slice(0, 19).replace(/[-:T]/g, '');
-
-console.log(build);
-params.push('--no-git-tag-version', '--new-version', build);
-
-const yarnPromise = util.promisify(yarn);
-
-async function yarnSync(params) {
-    let filePath = path.join('.', params[1], 'package.json');
-    let previous = fs.readFileSync(filePath);
-
-    let d = await yarnPromise(params);
-    if (branchName.startsWith('feature-')) {
-        await yarnPromise(['tag', 'add', json.name + '@' + build, name]);
+        process.exit(subprocess.status);
     }
 
-    fs.writeFileSync(filePath, previous);
+    return subprocess;
+};
 
-    return d;
+console.log('Releasing code from branch : ' + branchName + ' to ' + tag);
+
+// Ensure code is called from master
+// if (branchName !== 'master') {
+//     console.log('Can only release from master branch');
+//     process.exit(1);
+// }
+
+// Handle canary branches
+// let name = 'beta';
+// if (branchName.startsWith('feature-')) {
+//     name = branchName.replace(/-/g, '');
+//     params.push('--tag', name);
+// }
+//
+// build += '-' + name + '.' + (new Date()).toISOString().slice(0, 19).replace(/[-:T]/g, '');
+
+// Generate changelog
+spawnSync('yarn', ['auto', 'changelog', '--from', previous]);
+
+// Bump version
+let autoVersionProcess = spawnSync('yarn', ['auto', 'version', '--from', previous]);
+const result = autoVersionProcess.stdout.toString('ascii').split(/\r?\n/);
+const versionChange = result[1];
+
+if (versionChange !== 'patch' && versionChange !== 'minor' && versionChange !== 'major') {
+    console.log('No new version detected');
+    process.exit(0);
 }
 
-yarnSync(['publish', ...params]);
+console.log('Auto version change : ' + versionChange);
+spawnSync('npm', ['version', versionChange, '-m', 'Bump version to: %s [skip ci]']);
+
+// Publish
+spawnSync('npm', ['publish', buildPath]);
+
+// Push to repository
+spawnSync('git', ['push', '--follow-tags']);
+
+// Do git release
+// spawnSync('yarn', ['auto', 'release', '--from', previous]);
+
+console.log('Done');
