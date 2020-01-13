56084d354e52553e1d88341d6a3ed56c5d87f427
Florent Berthelot <FBerthelot@users.noreply.github.com>
2020-01-13 11:37:24 +0100

    BACKLOG-12055: supress react-i18next mock from test-framwork (#68)

--- packages/test-framework/src/setupTests.js
+++ packages/test-framework/src/setupTests.js
@@ -2,5 +2,3 @@ import {configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({adapter: new Adapter()});
-
-jest.mock('react-i18next', () => require('./__mocks__/react-i18next'));
