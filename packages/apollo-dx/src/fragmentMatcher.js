6ad6cb64f6a719516096ef249e65aac2dff2d81b
Florent Berthelot <FBerthelot@users.noreply.github.com>
2020-02-13 16:23:30 +0100

    BACKLOG-12345: remove apollo-dx project (#81)

--- packages/apollo-dx/src/fragmentMatcher.js
+++
@@ -1,8 +0,0 @@
-import {IntrospectionFragmentMatcher} from 'apollo-cache-inmemory';
-import introspectionQueryResultData from '../resources/fragmentTypes.json';
-
-const fragmentMatcher = new IntrospectionFragmentMatcher({
-    introspectionQueryResultData: introspectionQueryResultData
-});
-
-export {fragmentMatcher};
