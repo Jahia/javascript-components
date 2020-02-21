5c36fe8559efb15cb436c1d3a461f1cc70b9c605
Thomas Draier <tdraier@jahia.com>
2020-02-21 15:29:34 +0100

    BACKLOG-12488 : avoid multiple menu for same actionkey at the same time (#91)

--- packages/ui-extender/src/actions/menuAction/menuAction.jsx
+++ packages/ui-extender/src/actions/menuAction/menuAction.jsx
@@ -154,7 +154,7 @@ const reducer = (state, action) => {
 
 const MenuActionComponent = ({context, render: Render, loading: Loading}) => {
     const componentRenderer = useContext(ComponentRendererContext);
-    const id = 'actionComponent-' + context.id;
+    const id = 'actionComponent-' + context.actionKey;
     const {rootMenuContext, parentMenuContext} = context;
 
     const [menuState, dispatch] = useReducer(reducer, {
