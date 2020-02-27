d0fee69518586d31a10d7b48415fbcba07259be4
Thomas Draier <tdraier@jahia.com>
2020-02-27 14:53:35 +0100

    BACKLOG-12561 : removed useless class

--- packages/data-helper/src/hooks/useNodeInfo/useNodeInfo.gql-queries.js
+++ packages/data-helper/src/hooks/useNodeInfo/useNodeInfo.gql-queries.js
@@ -14,235 +14,229 @@ const NodeInfoQuery = gql`
     ${PredefinedFragments.nodeCacheRequiredFields.gql}
 `;
 
-export class NodeInfoQueryHandler {
-    constructor(variables, options = {}) {
-        this.fragments = [];
-        this.variables = {
-            path: variables.path
-        };
+export const getQuery = (variables, options = {}) => {
+    const fragments = [];
 
-        if (options.getDisplayName) {
-            this.fragments.push({
-                variables: {
-                    language: 'String!'
-                },
-                applyFor: 'node',
-                gql: gql`fragment NodeInfoDisplayName on JCRNode {
-                    displayName(language: $language)
-                }`
-            });
-            this.variables.language = variables.language;
-        }
+    const generatedVariables = {
+        path: variables.path
+    };
 
-        if (options.getPrimaryNodeType) {
-            this.fragments.push({
-                variables: {
-                    displayLanguage: 'String!'
-                },
-                applyFor: 'node',
-                gql: gql`fragment NodeInfoPrimaryNodeType on JCRNode {
-                    primaryNodeType {
-                        name
-                        displayName(language: $displayLanguage)
-                        icon
-                    }
-                }`
-            });
-            this.variables.displayLanguage = variables.displayLanguage;
-        }
-
-        if (options.getParent) {
-            this.fragments.push({
-                applyFor: 'node',
-                gql: gql`fragment NodeInfoParentNodeInfo on JCRNode {
-                    parent {
-                        path
-                        name
-                    }
-                }`
-            });
-        }
-
-        if (options.getAggregatedPublicationInfo) {
-            this.fragments.push({
-                variables: {
-                    language: 'String!'
-                },
-                applyFor: 'node',
-                gql: gql`fragment NodeInfoAggregatedPublicationInfo on JCRNode {
-                    aggregatedPublicationInfo(language: $language) {
-                        publicationStatus
-                    }
-                }`
-            });
-            this.variables.language = variables.language;
-        }
-
-        if (options.getOperationSupport) {
-            this.fragments.push({
-                applyFor: 'node',
-                gql: gql`fragment NodeInfoOperationSupport on JCRNode {
-                    operationsSupport {
-                        lock
-                        markForDeletion
-                        publication
-                    }
-                }`
-            });
-        }
-
-        if (options.getPermissions) {
-            let permissionNames = options.getPermissions;
-            this.fragments.push({
-                variables: permissionNames.map((name, idx) => idx).reduce((acc, idx) => Object.assign(acc, {['permission' + idx]: 'String!'}), {}),
-                applyFor: 'node',
-                gql: gql`fragment NodeInfoNodePermission on JCRNode {
-                    ${permissionNames.map((name, idx) => idx).reduce((acc, idx) => acc + ' ' + permissionNames[idx].replace(':', '_') + ':hasPermission(permissionName: $permission' + idx + ') ', '')}
-                }`
-            });
-            Object.assign(this.variables, permissionNames.map((name, idx) => idx).reduce((acc, idx) => Object.assign(acc, {['permission' + idx]: permissionNames[idx]}), {}));
-        }
-
-        if (options.getIsNodeTypes) {
-            let nodeTypes = options.getIsNodeTypes;
-            this.fragments.push({
-                variables: nodeTypes.map((name, idx) => idx).reduce((acc, idx) => Object.assign(acc, {['nodeType' + idx]: 'InputNodeTypesInput!'}), {}),
-                applyFor: 'node',
-                gql: gql`fragment NodeInfoNodeIsNodeType on JCRNode {
-                    ${nodeTypes.map((name, idx) => idx).reduce((acc, idx) => acc + ' ' + nodeTypes[idx].replace(':', '_') + ':isNodeType(type: $nodeType' + idx + ') ', '')}
-                }`
-            });
-            Object.assign(this.variables, nodeTypes.map((name, idx) => idx).reduce((acc, idx) => Object.assign(acc, {['nodeType' + idx]: {types: [nodeTypes[idx]]}}), {}));
-        }
-
-        if (options.getProperties) {
-            this.fragments.push({
-                variables: {
-                    getPropertiesNames: '[String!]!'
-                },
-                applyFor: 'node',
-                gql: gql`fragment NodeInfoNodeProperties on JCRNode {
-                    properties(names: $getPropertiesNames, language: $language) {
-                        name
-                        value
-                        values
-                    }
-                }`
-            });
-            this.variables.getPropertiesNames = options.getProperties;
-        }
-
-        if (options.getSiteInstalledModules) {
-            this.fragments.push({
-                applyFor: 'node',
-                gql: gql`fragment NodeInfoSiteInstalledModules on JCRNode {
-                    site {
-                        installedModulesWithAllDependencies
-                        ...NodeCacheRequiredFields
-                    }
-                }`
-            });
-        }
-
-        if (options.getSiteLanguages) {
-            this.fragments.push({
-                applyFor: 'node',
-                gql: gql`fragment NodeInfoSiteLanguages on JCRNode {
-                    site {
-                        defaultLanguage
-                        ...NodeCacheRequiredFields
-                        languages {
-                            displayName
-                            language
-                            activeInEdit
-                        }
-                    }
-                }`
-            });
-        }
-
-        if (options.getDisplayableNodePath) {
-            this.fragments.push({
-                applyFor: 'node',
-                gql: gql`fragment NodeInfoDisplayableNodePath on JCRNode {
-                    displayableNode {
-                        path
-                    }
-                }`
-            });
-        }
-
-        if (options.getLockInfo) {
-            this.fragments.push({
-                applyFor: 'node',
-                gql: gql`fragment NodeInfoLockInfo on JCRNode {
-                    lockOwner: property(name: "jcr:lockOwner") {
-                        value
-                    }
-                    lockTypes: property(name: "j:lockTypes") {
-                        values
-                    }
-                }`
-            });
-        }
-
-        if (options.getContributeTypesRestrictions) {
-            this.fragments.push({
-                applyFor: 'node',
-                gql: gql`fragment NodeInfoContentRestriction on JCRNode {
-                    contributeTypes: property(name: "j:contributeTypes") {
-                        values
-                    }
-                    ancestors(fieldFilter: {filters: {evaluation: NOT_EMPTY, fieldName: "contributeTypes"}}) {
-                        contributeTypes : property(name: "j:contributeTypes", language: $language) {
-                            values
-                        }
-                    }
-                }`
-            });
-        }
-
-        if (options.getSubNodesCount) {
-            this.fragments.push({
-                variables: {
-                    subNodesCountTypes: '[String!]!'
-                },
-                applyFor: 'node',
-                gql: gql`fragment NodeInfoSubNodesCount on JCRNode {
-                    subNodes: children(typesFilter: {types: $subNodesCountTypes, multi: ANY}) {
-                        pageInfo {
-                            totalCount
-                        }
-                    }
-                }`
-            });
-            this.variables.subNodesCountTypes = options.getSubNodesCount.types ? options.getSubNodesCount.types : ['nt:base'];
-        }
-
-        if (options.getMimeType) {
-            this.fragments.push({
-                applyFor: 'node',
-                gql: gql`fragment NodeInfoResourceNode on JCRNode {
-                    children(typesFilter: {types: ["jnt:resource"]}) {
-                        nodes {
-                            mimeType: property(name: "jcr:mimeType") {
-                                value
-                            }
-                        }
-                    }
-                }`
-            });
-        }
-
-        this.query = replaceFragmentsInDocument(NodeInfoQuery, this.fragments);
+    if (options.getDisplayName) {
+        fragments.push({
+            variables: {
+                language: 'String!'
+            },
+            applyFor: 'node',
+            gql: gql`fragment NodeInfoDisplayName on JCRNode {
+                displayName(language: $language)
+            }`
+        });
+        generatedVariables.language = variables.language;
     }
 
-    getQuery() {
-        return this.query;
+    if (options.getPrimaryNodeType) {
+        fragments.push({
+            variables: {
+                displayLanguage: 'String!'
+            },
+            applyFor: 'node',
+            gql: gql`fragment NodeInfoPrimaryNodeType on JCRNode {
+                primaryNodeType {
+                    name
+                    displayName(language: $displayLanguage)
+                    icon
+                }
+            }`
+        });
+        generatedVariables.displayLanguage = variables.displayLanguage;
     }
 
-    getVariables() {
-        return this.variables;
+    if (options.getParent) {
+        fragments.push({
+            applyFor: 'node',
+            gql: gql`fragment NodeInfoParentNodeInfo on JCRNode {
+                parent {
+                    path
+                    name
+                }
+            }`
+        });
     }
-}
 
+    if (options.getAggregatedPublicationInfo) {
+        fragments.push({
+            variables: {
+                language: 'String!'
+            },
+            applyFor: 'node',
+            gql: gql`fragment NodeInfoAggregatedPublicationInfo on JCRNode {
+                aggregatedPublicationInfo(language: $language) {
+                    publicationStatus
+                }
+            }`
+        });
+        generatedVariables.language = variables.language;
+    }
+
+    if (options.getOperationSupport) {
+        fragments.push({
+            applyFor: 'node',
+            gql: gql`fragment NodeInfoOperationSupport on JCRNode {
+                operationsSupport {
+                    lock
+                    markForDeletion
+                    publication
+                }
+            }`
+        });
+    }
+
+    if (options.getPermissions) {
+        let permissionNames = options.getPermissions;
+        fragments.push({
+            variables: permissionNames.map((name, idx) => idx).reduce((acc, idx) => Object.assign(acc, {['permission' + idx]: 'String!'}), {}),
+            applyFor: 'node',
+            gql: gql`fragment NodeInfoNodePermission on JCRNode {
+                ${permissionNames.map((name, idx) => idx).reduce((acc, idx) => acc + ' ' + permissionNames[idx].replace(':', '_') + ':hasPermission(permissionName: $permission' + idx + ') ', '')}
+            }`
+        });
+        Object.assign(generatedVariables, permissionNames.map((name, idx) => idx).reduce((acc, idx) => Object.assign(acc, {['permission' + idx]: permissionNames[idx]}), {}));
+    }
+
+    if (options.getIsNodeTypes) {
+        let nodeTypes = options.getIsNodeTypes;
+        fragments.push({
+            variables: nodeTypes.map((name, idx) => idx).reduce((acc, idx) => Object.assign(acc, {['nodeType' + idx]: 'InputNodeTypesInput!'}), {}),
+            applyFor: 'node',
+            gql: gql`fragment NodeInfoNodeIsNodeType on JCRNode {
+                ${nodeTypes.map((name, idx) => idx).reduce((acc, idx) => acc + ' ' + nodeTypes[idx].replace(':', '_') + ':isNodeType(type: $nodeType' + idx + ') ', '')}
+            }`
+        });
+        Object.assign(generatedVariables, nodeTypes.map((name, idx) => idx).reduce((acc, idx) => Object.assign(acc, {['nodeType' + idx]: {types: [nodeTypes[idx]]}}), {}));
+    }
+
+    if (options.getProperties) {
+        fragments.push({
+            variables: {
+                getPropertiesNames: '[String!]!'
+            },
+            applyFor: 'node',
+            gql: gql`fragment NodeInfoNodeProperties on JCRNode {
+                properties(names: $getPropertiesNames, language: $language) {
+                    name
+                    value
+                    values
+                }
+            }`
+        });
+        generatedVariables.getPropertiesNames = options.getProperties;
+    }
+
+    if (options.getSiteInstalledModules) {
+        fragments.push({
+            applyFor: 'node',
+            gql: gql`fragment NodeInfoSiteInstalledModules on JCRNode {
+                site {
+                    installedModulesWithAllDependencies
+                    ...NodeCacheRequiredFields
+                }
+            }`
+        });
+    }
+
+    if (options.getSiteLanguages) {
+        fragments.push({
+            applyFor: 'node',
+            gql: gql`fragment NodeInfoSiteLanguages on JCRNode {
+                site {
+                    defaultLanguage
+                    ...NodeCacheRequiredFields
+                    languages {
+                        displayName
+                        language
+                        activeInEdit
+                    }
+                }
+            }`
+        });
+    }
+
+    if (options.getDisplayableNodePath) {
+        fragments.push({
+            applyFor: 'node',
+            gql: gql`fragment NodeInfoDisplayableNodePath on JCRNode {
+                displayableNode {
+                    path
+                }
+            }`
+        });
+    }
+
+    if (options.getLockInfo) {
+        fragments.push({
+            applyFor: 'node',
+            gql: gql`fragment NodeInfoLockInfo on JCRNode {
+                lockOwner: property(name: "jcr:lockOwner") {
+                    value
+                }
+                lockTypes: property(name: "j:lockTypes") {
+                    values
+                }
+            }`
+        });
+    }
+
+    if (options.getContributeTypesRestrictions) {
+        fragments.push({
+            applyFor: 'node',
+            gql: gql`fragment NodeInfoContentRestriction on JCRNode {
+                contributeTypes: property(name: "j:contributeTypes") {
+                    values
+                }
+                ancestors(fieldFilter: {filters: {evaluation: NOT_EMPTY, fieldName: "contributeTypes"}}) {
+                    contributeTypes : property(name: "j:contributeTypes", language: $language) {
+                        values
+                    }
+                }
+            }`
+        });
+    }
+
+    if (options.getSubNodesCount) {
+        fragments.push({
+            variables: {
+                subNodesCountTypes: '[String!]!'
+            },
+            applyFor: 'node',
+            gql: gql`fragment NodeInfoSubNodesCount on JCRNode {
+                subNodes: children(typesFilter: {types: $subNodesCountTypes, multi: ANY}) {
+                    pageInfo {
+                        totalCount
+                    }
+                }
+            }`
+        });
+        generatedVariables.subNodesCountTypes = options.getSubNodesCount.types ? options.getSubNodesCount.types : ['nt:base'];
+    }
+
+    if (options.getMimeType) {
+        fragments.push({
+            applyFor: 'node',
+            gql: gql`fragment NodeInfoResourceNode on JCRNode {
+                children(typesFilter: {types: ["jnt:resource"]}) {
+                    nodes {
+                        mimeType: property(name: "jcr:mimeType") {
+                            value
+                        }
+                    }
+                }
+            }`
+        });
+    }
+
+    return {
+        query: replaceFragmentsInDocument(NodeInfoQuery, fragments),
+        generatedVariables,
+        fragments
+    };
+};
--- packages/data-helper/src/hooks/useNodeInfo/useNodeInfo.js
+++ packages/data-helper/src/hooks/useNodeInfo/useNodeInfo.js
@@ -1,12 +1,12 @@
 import {useRef} from 'react';
 import {useQuery} from 'react-apollo';
-import {NodeInfoQueryHandler} from './useNodeInfo.gql-queries';
+import {getQuery} from './useNodeInfo.gql-queries';
 
 export const useNodeInfo = (variables, options) => {
     // Use ref to avoid infinite loop, as query object will be regenerated every time
-    const {current: queryHandler} = useRef(new NodeInfoQueryHandler(variables, options));
+    const {current: {query, generatedVariables, fragments}} = useRef(getQuery(variables, options));
 
-    const {data, ...others} = useQuery(queryHandler.getQuery(), {variables: queryHandler.getVariables(), skip: queryHandler.fragments.length === 0});
+    const {data, ...others} = useQuery(query, {variables: generatedVariables, skip: fragments.length === 0});
 
     const node = (data && data.jcr && data.jcr.nodeByPath) || null;
 
