7daae6ab34ac180a1b2636a0d28a202b5da90ad7
Thomas Draier <tdraier@jahia.com>
2020-04-15 11:03:32 +0200

    Optimize component renderer by splitting in different components

---
+++ packages/ui-extender/src/ComponentRenderer/ComponentRendererContext.jsx
@@ -0,0 +1,6 @@
+import {createContext} from 'react';
+
+export const ComponentRendererContext = createContext({});
+
+export const ComponentRendererConsumer = ComponentRendererContext.Consumer;
+
--- packages/ui-extender/src/ComponentRenderer/index.js
+++ packages/ui-extender/src/ComponentRenderer/index.js
@@ -1,2 +1,4 @@
 export * from './ComponentRenderer';
+export * from './ComponentRendererContext';
+export * from './ComponentRendererProvider';
 
