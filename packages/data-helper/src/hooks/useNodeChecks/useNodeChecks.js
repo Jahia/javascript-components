535a0b7e5ce3f5455f02b76e20b6cd17b556b37a
Kevan <kevan@jahia.com>
2020-05-01 11:48:09 +0200

    BACKLOG-13212: correcly set node info options in useNodeChecks hook (missing code formats)

--- packages/data-helper/src/hooks/useNodeChecks/useNodeChecks.js
+++ packages/data-helper/src/hooks/useNodeChecks/useNodeChecks.js
@@ -18,10 +18,11 @@ export const useNodeChecks = (variables, options, queryOptions) => {
 
     function arrayUnique(array) {
         let a = array.concat();
-        for(let i=0; i<a.length; ++i) {
-            for(let j=i+1; j<a.length; ++j) {
-                if(a[i] === a[j])
+        for (let i = 0; i < a.length; ++i) {
+            for (let j = i + 1; j < a.length; ++j) {
+                if (a[i] === a[j]) {
                     a.splice(j--, 1);
+                }
             }
         }
 
