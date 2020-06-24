5ab44673723422a0afbecef0438c426ee516da21
cedmail <cmailleux@jahia.com>
2020-06-24 13:13:52 -0400

    Guild day, use @testing-library/react instaed of enzyme for react components testing

--- packages/ui-extender/src/IframeRenderer/IframeRenderer.spec.jsx
+++ packages/ui-extender/src/IframeRenderer/IframeRenderer.spec.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {render} from '@testing-library/react';
 import {getIframeRenderer, IframeRenderer} from './IframeRenderer';
+import '@testing-library/jest-dom';
 
 describe('IframeRenderer', () => {
     it('should render', () => {
-        const component = shallow(<IframeRenderer url="/goku/cms/adminframe/default/en/sites/toto.linkChecker.html"/>);
-        expect(component.html()).toBe('<iframe src="/goku/cms/adminframe/default/en/sites/toto.linkChecker.html" style="display:block" width="100%" height="100%"></iframe>');
+        const {container} = render(<IframeRenderer url="/goku/cms/adminframe/default/en/sites/toto.linkChecker.html"/>);
+        let actual = container.querySelector('iframe');
+        expect(actual).toHaveAttribute('src', '/goku/cms/adminframe/default/en/sites/toto.linkChecker.html');
+        expect(actual).toHaveAttribute('style', 'display: block;');
+        expect(actual).toHaveAttribute('width', '100%');
+        expect(actual).toHaveAttribute('height', '100%');
     });
 
     it('should render when using function call', () => {
-        const component = shallow(getIframeRenderer('/goku/cms/adminframe/default/en/sites/toto.linkChecker.html'));
-        expect(component.html()).toBe('<iframe src="/goku/cms/adminframe/default/en/sites/toto.linkChecker.html" style="display:block" width="100%" height="100%"></iframe>');
+        const {container} = render(getIframeRenderer('/goku/cms/adminframe/default/en/sites/toto.linkChecker.html'));
+        let actual = container.querySelector('iframe');
+        expect(actual).toHaveAttribute('src', '/goku/cms/adminframe/default/en/sites/toto.linkChecker.html');
+        expect(actual).toHaveAttribute('style', 'display: block;');
+        expect(actual).toHaveAttribute('width', '100%');
+        expect(actual).toHaveAttribute('height', '100%');
     });
 });
