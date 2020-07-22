013e48e721d5c8738a8351b1350b7d27a340b4bf
Yousria <ybenchadi@jahia.com>
2020-07-22 13:59:00 +0200

    QA-13044 (#155)

--- packages/data-helper/src/hooks/useSchemaFields/useSchemaFields.jsx
+++ packages/data-helper/src/hooks/useSchemaFields/useSchemaFields.jsx
@@ -1,6 +1,6 @@
 import {useQuery} from 'react-apollo';
 import {SCHEMA_FIELDS_QUERY} from './useSchemaFields.gql-queries';
 
-export const useSchemaFields = () => {
-    return useQuery(SCHEMA_FIELDS_QUERY);
+export const useSchemaFields = variables => {
+    return useQuery(SCHEMA_FIELDS_QUERY, {variables: variables});
 };
