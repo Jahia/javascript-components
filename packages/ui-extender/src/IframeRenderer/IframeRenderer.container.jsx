ef4c288df8af0400a34abd76b534c5836a713d86
Thomas Draier <tdraier@jahia.com>
2020-11-03 17:12:21 +0100

    QA-13109: Implement a simple waitInProgress method (#176)

--- packages/ui-extender/src/IframeRenderer/IframeRenderer.container.jsx
+++ packages/ui-extender/src/IframeRenderer/IframeRenderer.container.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useSelector} from 'react-redux';
 import PropTypes from 'prop-types';
 import {parseUrl} from './IframeRenderer.utils';
 import {IframeRenderer} from './IframeRenderer';
+
+const Progress = ({text}) => (
+    <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0,0,0,.8)',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%'
+    }}
+    >
+        <span style={{color: 'white'}}>{text}</span>
+    </div>
+);
+
+Progress.propTypes = {
+    text: PropTypes.string.isRequired
+};
 
 export const IframeRendererContainer = props => {
     const {siteKey, uiLang, language} = useSelector(state => ({
@@ -10,12 +29,31 @@ export const IframeRendererContainer = props => {
         uiLang: state.uilang,
         language: state.language
     }));
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        window.waitingMask = message => {
+            setLoading(message);
+        };
+
+        return () => {
+            delete window.waitingMask;
+        };
+    });
 
     return (
-        <IframeRenderer width="100%"
-                        height="100%"
-                        {...props}
-                        url={parseUrl(props.url, siteKey, language, uiLang)}/>
+        <div style={{flex: 1, display: 'flex', position: 'relative'}}>
+            {loading && <Progress text={loading}/>}
+            <IframeRenderer width="100%"
+                            height="100%"
+                            onLoad={() => {
+                                if (loading) {
+                                    setLoading(false);
+                                }
+                            }}
+                            {...props}
+                            url={parseUrl(props.url, siteKey, language, uiLang)}/>
+        </div>
     );
 };
 
