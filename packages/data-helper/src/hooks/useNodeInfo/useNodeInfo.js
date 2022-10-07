4acce04290fd91af3b2a6fe7a951ad22970d9736
Thomas Draier <tdraier@jahia.com>
2022-10-07 11:01:42 +0200

    Improved watch

--- packages/data-helper/src/hooks/useNodeInfo/useNodeInfo.js
+++ packages/data-helper/src/hooks/useNodeInfo/useNodeInfo.js
@@ -1,55 +1,140 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {useApolloClient} from 'react-apollo';
-import {getQuery, validOptions} from './useNodeInfo.gql-queries';
+import {getQuery} from './useNodeInfo.gql-queries';
 import {getEncodedPermissionName} from '../../fragments/getPermissionFragment';
 import {getEncodedNodeTypeName} from '../../fragments/getIsNodeTypeFragment';
 import {SCHEMA_FIELDS_QUERY} from '../useSchemaFields/useSchemaFields.gql-queries';
+import {isSubset, merge} from './useNodeInfo.utils';
+import {useMemoRequest} from './useMemoRequest';
+import deepEquals from 'fast-deep-equal';
 
 let queue = [];
 let schemaResult;
 let timeout;
+let observedQueries = [];
 
-function isObject(obj) {
-    return obj !== null && typeof obj === 'object';
+function scheduleQueue(client) {
+    if (!timeout && schemaResult) {
+        timeout = setTimeout(() => {
+            timeoutHandler(client);
+            timeout = null;
+        }, 0);
+    }
 }
 
-const merge = (target, source) => {
-    if (Array.isArray(target) && Array.isArray(source)) {
-        target.push(...source.filter(f => target.indexOf(f) === -1));
-        return target;
-    }
+const timeoutHandler = client => {
+    const mergedQueue = [];
+    console.log("Merging " + queue.length + " queries");
+    const start = Date.now();
 
-    if (isObject(target) && isObject(source)) {
-        Object.keys(source).forEach(sourceKey => {
-            const sourceValue = source[sourceKey];
-            if (Object.prototype.hasOwnProperty.call(target, sourceKey)) {
-                const targetValue = target[sourceKey];
-                target[sourceKey] = merge(targetValue, sourceValue);
-            } else {
-                target[sourceKey] = sourceValue;
-            }
-        });
+    queue.forEach(request => {
+        const toInsert = {
+            variables: request.variables,
+            queryOptions: request.queryOptions,
+            options: request.options,
+            originals: [request]
+        };
 
-        return target;
-    }
+        const mergeable = mergedQueue.find(q => JSON.stringify(q.queryOptions) === JSON.stringify(toInsert.queryOptions) && (isSubset(q.variables, toInsert.variables) || isSubset(toInsert.variables, q.variables)));
 
-    return target;
-};
-
-const isSubset = (superObj, subObj) => {
-    return Object.keys(subObj).every(ele => {
-        if (typeof subObj[ele] === 'object' && !Array.isArray(subObj[ele])) {
-            return isSubset(superObj[ele], subObj[ele]);
+        if (mergeable) {
+            merge(mergeable, toInsert);
+        } else {
+            mergedQueue.push({
+                variables: toInsert.variables && {...toInsert.variables},
+                queryOptions: toInsert.queryOptions && {...toInsert.queryOptions},
+                options: toInsert.options && {...toInsert.options},
+                originals: toInsert.originals
+            });
         }
+    });
 
-        return subObj[ele] === superObj[ele];
+    observedQueries.forEach(obs => obs.unsubscribe());
+    observedQueries = [];
+
+    const millis = Date.now() - start;
+
+    console.log("Merged : " + mergedQueue.length + " in " + millis + "ms", mergedQueue);
+
+    mergedQueue.forEach(mergedRequest => {
+        const {variables, queryOptions, options, originals} = mergedRequest;
+        const {query, generatedVariables, skip} = getQuery(variables, schemaResult, options);
+        if (skip) {
+            // No query to execute
+            originals.forEach(request => {
+                request.setResult({
+                    loading: false
+                });
+            });
+        } else {
+            const watchedQuery = client.watchQuery({query, errorPolicy: 'ignore', ...queryOptions, variables: generatedVariables}).subscribe(({data, ...others}) => {
+                const result = getResult(data, others, options, query, generatedVariables);
+
+                originals.forEach(request => {
+                    if (!deepEquals(request.result, result)) {
+                        request.result = result;
+                        request.setResult({
+                            ...result,
+                            refetch: () => {
+                                client.refetchQueries({include: [query]});
+                            }
+                        });
+                    }
+                });
+            });
+            observedQueries.push(watchedQuery);
+        }
     });
 };
 
-const clean = obj => obj && Object.fromEntries(Object.entries(obj).filter(([_, v]) => v !== null && v !== undefined));
-const cleanOptions = obj => obj && Object.fromEntries(Object.entries(obj).filter(([k, v]) => v !== null && v !== undefined && validOptions.indexOf(k) !== -1));
+export const useNodeInfo = (variables, options, queryOptions) => {
+    const [result, setResult] = useState({
+        loading: true
+    });
 
-function getResult(data, others, options, query, generatedVariables) {
+    const client = useApolloClient();
+
+    if (!schemaResult) {
+        client.query({query: SCHEMA_FIELDS_QUERY, variables: {type: 'GqlPublicationInfo'}}).then(({data}) => {
+            schemaResult = data;
+            scheduleQueue(client);
+        });
+    }
+
+    // Normalize and memoize request
+    const [currentRequest, changed] = useMemoRequest(variables, queryOptions, options, setResult);
+    useEffect(() => {
+        queue.push(currentRequest);
+        scheduleQueue(client);
+
+        return () => {
+            queue.splice(queue.indexOf(currentRequest), 1);
+        };
+    }, [currentRequest]);
+
+    // if (changed && schemaResult) {
+    //     // Use cache only if schemaResult is available
+    //     const {query, generatedVariables, skip} = getQuery(currentRequest.variables, schemaResult, currentRequest.options);
+    //     if (skip) {
+    //         console.log('skip ', currentRequest);
+    //         return {
+    //             loading: false
+    //         };
+    //     }
+    //
+    //     const cachedData = client.readQuery({query, ...currentRequest.queryOptions, variables: generatedVariables});
+    //     if (cachedData) {
+    //         console.log('from cache ', currentRequest);
+    //         currentRequest.cachedResult = true;
+    //         const refetch = () => {};
+    //         return getResult(cachedData, {loading: false, refetch}, currentRequest.options, query, generatedVariables);
+    //     }
+    // }
+
+    return result;
+};
+
+const getResult = (data, others, options, query, generatedVariables) => {
     const node = (data && data.jcr && (data.jcr.nodeByPath || data.jcr.nodeById)) || null;
     const nodes = (data && data.jcr && (data.jcr.nodesByPath || data.jcr.nodesById)) || null;
     let result = others;
@@ -70,109 +155,6 @@ function getResult(data, others, options, query, generatedVariables) {
             query,
             variables: generatedVariables
         };
-    }
-
-    return result;
-}
-
-const timeoutHandler = client => {
-    const mergedQueue = [];
-    queue.forEach(value => {
-        const toInsert = {
-            variables: clean(value.variables),
-            queryOptions: clean(value.queryOptions),
-            options: cleanOptions(value.options),
-            originals: [value]
-        };
-
-        const mergeable = mergedQueue.find(q => JSON.stringify(q.queryOptions) === JSON.stringify(toInsert.queryOptions) && (isSubset(q.variables, toInsert.variables) || isSubset(toInsert.variables, q.variables)));
-
-        if (mergeable) {
-            merge(mergeable, toInsert);
-        } else {
-            mergedQueue.push(toInsert);
-        }
-    });
-
-    mergedQueue.forEach(value => {
-        const {variables, queryOptions, options, originals} = value;
-        const {query, generatedVariables, skip} = getQuery(variables, schemaResult, options);
-        if (skip) {
-            // No query to execute
-            originals.forEach(value => {
-                value.setResult({
-                    loading: false
-                });
-            });
-        } else {
-            client.query({query, errorPolicy: 'ignore', ...queryOptions, variables: generatedVariables}).then(({data, ...others}) => {
-                const result = getResult(data, others, options, query, generatedVariables);
-
-                originals.forEach(value => {
-                    value.setResult({
-                        ...result,
-                        refetch: () => {
-                            queue.push({...value, queryOptions: {...value.queryOptions, fetchPolicy: 'network-only'}});
-                            scheduleQueue(client);
-                        }
-                    });
-                });
-            });
-        }
-    });
-
-    queue = [];
-    timeout = null;
-};
-
-function scheduleQueue(client) {
-    if (!timeout && schemaResult) {
-        timeout = setTimeout(() => {
-            timeoutHandler(client);
-        }, 0);
-    }
-}
-
-export const useNodeInfo = (variables, options, queryOptions) => {
-    const [result, setResult] = useState({
-        loading: true
-    });
-
-    const client = useApolloClient();
-
-    if (!schemaResult) {
-        client.query({query: SCHEMA_FIELDS_QUERY, variables: {type: 'GqlPublicationInfo'}}).then(({data}) => {
-            schemaResult = data;
-            scheduleQueue(client);
-        });
-    }
-
-    if (!result.queued && result.loading) {
-        if (schemaResult) {
-            // Use cache only if schemaResult is available
-            const {query, generatedVariables, skip} = getQuery(variables, schemaResult, options);
-            if (skip) {
-                return {
-                    loading: false
-                };
-            }
-
-            const cachedData = client.readQuery({query, ...queryOptions, variables: generatedVariables});
-            if (cachedData) {
-                const refetch = () => {
-                    queue.push({variables, queryOptions: {...queryOptions, fetchPolicy: 'network-only'}, options, setResult});
-                    scheduleQueue(client);
-                    console.log('refetch not implemented (cached data)', variables, options);
-                };
-
-                return getResult(cachedData, {loading: false, refetch}, options, query, generatedVariables);
-            }
-        }
-
-        queue.push({variables, queryOptions, options, setResult});
-        scheduleQueue(client);
-
-        setResult({...result, queued: true});
     }
 
     return result;
