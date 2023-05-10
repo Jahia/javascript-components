93ac65761ff8d7786be43671156ecbd9634a865a
Thomas Draier <tdraier@jahia.com>
2023-05-10 17:34:06 +0200

    TECH-233: use single entrypoint for js and ts, generate cjs and mjs (#241)

--- packages/scripts/babel-module.js
+++ packages/scripts/babel-module.js
@@ -5,5 +5,6 @@ console.log('Transpiling for esm');
 
 babel('build/esm', {
     presets: [['@babel/env', {modules: false}], '@babel/react'],
+    sourceMaps: true,
     plugins: ['lodash', '@babel/plugin-syntax-dynamic-import']
 });
--- packages/scripts/babel.js
+++ packages/scripts/babel.js
@@ -6,22 +6,30 @@ const glob = require('glob');
 const fx = require('mkdir-recursive');
 
 function transform(buildFolder, conf) {
-    const files = [...glob.sync('**/*.js', {
-        ignore: '*.test.js',
-        cwd: 'src'
-    }), ...glob.sync('**/*.jsx', {ignore: '*.test.jsx', cwd: 'src'})];
+    const files = [
+        ...glob.sync('**/*.js', {ignore: '**/*.test.js', cwd: 'src'}),
+        ...glob.sync('**/*.jsx', {ignore: '**/*.test.jsx', cwd: 'src'}),
+        ...glob.sync('**/*.ts', {ignore: ['**/*.test.ts', '**/*.d.ts'], cwd: 'src'}),
+        ...glob.sync('**/*.tsx', {ignore: '**/*.test.tsx', cwd: 'src'})
+    ];
 
     files.forEach(file => {
-        let result = babel.transformFileSync(path.resolve('src', file), conf);
+        const result = babel.transformFileSync(path.resolve('src', file), conf);
 
-        let target = path.resolve(buildFolder, file);
-        let folder = path.resolve(target, '..');
+        const target = path.resolve(buildFolder, file).replace(/\.[tj]sx?$/, '.js');
+        const folder = path.resolve(target, '..');
 
         if (!fs.existsSync(folder)) {
             fx.mkdirSync(folder);
         }
 
+        if (result.map) {
+            result.code += '\n' + result.map.sources.map(m => '//# sourceMappingURL=' + m.replace(/\.[tj]sx?$/, '.js') + '.map\n');
+        }
+
         fs.writeFileSync(target, result.code);
+        fs.writeFileSync(target + '.map', JSON.stringify(result.map));
+
         console.log('Transpiled file ' + target);
     });
 }
