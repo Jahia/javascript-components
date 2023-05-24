f95e936f1b68c9dc5ffeae03f70060b59cb37cb8
Thomas Draier <tdraier@jahia.com>
2023-05-24 12:42:55 +0200

    make build scripts more generic

--- packages/scripts/prepare-build.js
+++ packages/scripts/prepare-build.js
@@ -1,28 +1,9 @@
 #!/usr/bin/env node
-const path = require('path');
-const fs = require('fs');
+
+const copyFile = require('./copy-file');
 const glob = require('glob');
 
 console.log('Preparing build folder');
-
-function mkdirs(dir) {
-    if (!fs.existsSync(dir)) {
-        let parent = path.resolve(dir, '..');
-        mkdirs(parent);
-        fs.mkdirSync(dir);
-    }
-}
-
-function copyFile(srcFolder, destFolder, file, newFile) {
-    let source = path.resolve(srcFolder, file);
-    if (fs.lstatSync(source).isFile()) {
-        newFile = newFile || file;
-        let target = path.resolve(destFolder, newFile);
-        mkdirs(path.resolve(target, '..'));
-        fs.createReadStream(source).pipe(fs.createWriteStream(target));
-        console.log(`Copied ${source} to ${target}`);
-    }
-}
 
 function typingsCopy() {
     // Create index.d.ts
