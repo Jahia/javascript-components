7eb72deb4effeb22219da3b929e3657688427b48
Thomas Draier <tdraier@jahia.com>
2023-05-24 10:44:02 +0200

    Share @apollo/client

--- packages/webpack-config/getModuleFederationConfig.js
+++ packages/webpack-config/getModuleFederationConfig.js
@@ -28,6 +28,7 @@ const sharedDeps = [
     '@jahia/data-helper',
 
     // Apollo
+    '@apollo/client',
     '@apollo/react-common',
     '@apollo/react-components',
     '@apollo/react-hooks',
@@ -55,6 +56,7 @@ const singletonDeps = [
     'redux',
     '@jahia/moonstone',
     '@jahia/ui-extender',
+    '@apollo/client',
     '@apollo/react-common',
     '@apollo/react-components',
     '@apollo/react-hooks',
