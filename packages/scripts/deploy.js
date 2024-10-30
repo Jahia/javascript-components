f87c9a99092ae6029592e82f5fe093286d398785
Baptiste Grimaud <bgrimaud@jahia.com>
2024-10-30 11:19:00 +0100

    BACKLOG-23333: Use dist/build folder for generated code

--- packages/scripts/deploy.js
+++ packages/scripts/deploy.js
@@ -2,13 +2,16 @@
 require('dotenv').config();
 const {execSync} = require('child_process');
 const pack = require('./pack-project');
-const {getPackageFilename} = require("./pack-utility");
+const {getPackageFilename} = require('./pack-utility');
 const fs = require('fs');
-const path = require("path");
+const path = require('path');
 
 const args = process.argv.slice(2);
 const deployMethod = process.env.JAHIA_DEPLOY_METHOD;
 const packageFileName = getPackageFilename(path.join(process.cwd(), 'package.json'), process.env.npm_package_name, process.env.npm_package_version);
+const buildDir = path.resolve('dist/build');
+const packDir = fs.existsSync(buildDir) ? path.resolve('dist') : process.cwd();
+const packageFilePath = path.join(packDir, packageFileName);
 
 if (!fs.existsSync(packageFileName)) {
     console.log('Package hasn\'t been built previously, packing now using jahia-pack...');
@@ -20,8 +23,8 @@ if (!fs.existsSync(packageFileName)) {
 
 if (deployMethod === 'curl') {
     console.log('Deploying URL curl to Jahia bundles REST API...');
-    console.log(execSync(`curl -s --user ${process.env.JAHIA_USER} --form bundle=@./${packageFileName} --form start=true ${process.env.JAHIA_HOST}/modules/api/bundles`, {encoding: 'utf8'}));
+    console.log(execSync(`curl -s --user ${process.env.JAHIA_USER} --form bundle=@./${packageFilePath} --form start=true ${process.env.JAHIA_HOST}/modules/api/bundles`, {encoding: 'utf8'}));
 } else {
     console.log('Deploying using Docker copy...');
-    console.log(execSync(`docker cp ${packageFileName} ${process.env.JAHIA_DOCKER_NAME}:/var/jahia/modules`, {encoding: 'utf8'}));
+    console.log(execSync(`docker cp ${packageFilePath} ${process.env.JAHIA_DOCKER_NAME}:/var/jahia/modules`, {encoding: 'utf8'}));
 }
