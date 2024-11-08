3f12fc9a3ff101474a2993b192ba57b0fc305712
Baptiste Grimaud <bgrimaud@jahia.com>
2024-11-08 14:55:17 +0100

    BACKLOG-23333: Use dist/build folder for generated code (#279)
    Update the pack-project.js script (used by yarn build) to pack the content of the dist/build/ folder into a dist/<project>-<version>.tgz file.
    If the dist/build folder does not exist, fallback to the previous behaviour and pack the root directory directly.

--- packages/scripts/pack-project.js
+++ packages/scripts/pack-project.js
@@ -1,22 +1,24 @@
 const {execSync} = require('child_process');
 const semver = require('semver');
 const path = require('path');
-const {getPackageFilename} = require("./pack-utility");
-
-
+const {getPackageFilename} = require('./pack-utility');
+const fs = require('fs');
 
 function pack() {
     console.log('Node version detected:', process.versions.node);
     const yarnVersion = execSync('yarn --version', {encoding: 'utf8'});
     console.log('Yarn version:', yarnVersion);
+    const buildDir = path.resolve('dist/build');
+    const packDir = fs.existsSync(buildDir) ? buildDir : process.cwd();
+    console.log(`Packing folder ${packDir}...`);
 
     if (semver.satisfies(yarnVersion, '1.x')) {
         console.log('Yarn Classic detected');
         console.log(execSync('yarn pack', {encoding: 'utf8'}));
     } else if (semver.gte(yarnVersion, '2.0.0')) {
         console.log('Yarn Berry detected');
-        const outputFileName = getPackageFilename(path.join(process.cwd(), 'package.json'),process.env.npm_package_name,process.env.npm_package_version);
-        console.log(execSync(`yarn pack --out ${outputFileName}`, {encoding: 'utf8'}));
+        const outputFileName = getPackageFilename(path.join(process.cwd(), 'package.json'), process.env.npm_package_name, process.env.npm_package_version);
+        console.log(execSync(`cd ${packDir} && yarn pack --out ${outputFileName} && mv ${outputFileName} ..`, {encoding: 'utf8'}));
     }
 }
 
