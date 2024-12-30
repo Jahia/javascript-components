7077712f55c7a32066b6fdca7f57165044d83bd3
Baptiste Grimaud <bgrimaud@jahia.com>
2024-12-30 09:21:06 +0100

    QA-15406: Fix curl deployment (yarn watch/deploy)

--- packages/scripts/deploy.js
+++ packages/scripts/deploy.js
@@ -23,7 +23,7 @@ if (!fs.existsSync(packageFileName)) {
 
 if (deployMethod === 'curl') {
     console.log('Deploying URL curl to Jahia bundles REST API...');
-    console.log(execSync(`curl -s --user ${process.env.JAHIA_USER} --form bundle=@./${packageFilePath} --form start=true ${process.env.JAHIA_HOST}/modules/api/bundles`, {encoding: 'utf8'}));
+    console.log(execSync(`curl -s --user ${process.env.JAHIA_USER} --form bundle=@${packageFilePath} --form start=true ${process.env.JAHIA_HOST}/modules/api/bundles`, {encoding: 'utf8'}));
 } else {
     console.log('Deploying using Docker copy...');
     console.log(execSync(`docker cp ${packageFilePath} ${process.env.JAHIA_DOCKER_NAME}:/var/jahia/modules`, {encoding: 'utf8'}));
